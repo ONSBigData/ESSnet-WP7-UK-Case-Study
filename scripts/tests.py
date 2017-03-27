7fc4c578a9c1a93485a4ce06eb2bb5349d9c0025
Alessandra Sozzi <alessandra.sozzi@gmail.com>
2017-03-27 09:10:41 +0200

    save data

--- scripts/tests.py
+++ scripts/tests.py
@@ -1,11 +1,12 @@
 import json
 import pandas as pd
-from fb_api import Mongo
 from pandas.io.json import json_normalize
 
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn; seaborn.set()
+
+from nltk import sent_tokenize
 
 
 def d_to_df(d):
@@ -19,7 +20,14 @@ docs = [doc for doc in mongo.collection.find()]
 mongo.close()
 mongo_ids = [doc.pop('_id', None) for doc in docs] # exclude mongo generated ids
 
+mongo = Mongo('facebook', 'posts')
+posts = [doc for doc in mongo.collection.find()]
+mongo.close()
+mongo_ids = [post.pop('_id', None) for post in posts] # exclude mongo generated ids
 
+
+
+posts = d_to_df(posts)
 docs = d_to_df(docs)
 docs['created_time'] = pd.to_datetime(docs['created_time'],format="%Y-%m-%dT%H:%M:%S+0000")
 docs.set_index('created_time', inplace = True)
@@ -38,9 +46,9 @@ docs['day'] = [x.date() for x in docs.index]
 by_hw = docs.groupby(['day','hour']).count().reset_index()
 
 fig = plt.figure(figsize=(20,10))
-NUM_WEEKS = 3
+NUM_WEEKS = 4
 axes = [plt.subplot(1,NUM_WEEKS,i) for i in range(1, NUM_WEEKS + 1)]
-idx = [0, 168, 336, 492] # need to fix this with 7 days range
+idx = [0, 168, 336, 492, 608] # need to fix this with 7 days range
 for i in range(NUM_WEEKS):
     ax = axes[i]
     week = by_hw.iloc[idx[i]:idx[i+1]].copy()
@@ -52,3 +60,36 @@ for i in range(NUM_WEEKS):
     ax.set_xticks(np.arange(0, 7, 1));
     ax.set_xticklabels(['Mon','Tue','Wed','Thu','Fri','Sat','Sun'])
     ax.grid(None)
+
+docs['n_sents'] = docs.message.apply(lambda x: len(sent_tokenize(x)))
+
+
+import time
+
+start = time.time()
+scores = get_nrc_scores(docs['message'])
+end = time.time()
+print(end - start) # 10 secs
+
+
+docs['pos'] = scores[:,0]
+docs['neg'] = scores[:,1]
+
+docs[docs['parent_id'].isnull()][['pos', 'neg']].resample('D').mean().plot()
+
+
+p = posts[posts['main_category'] == 'politics']['post_id']
+docs[docs['post_id'].isin(p)][['pos', 'neg']].resample('H').mean().plot()
+
+posts.groupby('main_category').count()['categories'].plot(kind='barh')
+
+docs.reset_index()[['created_time', u'comment_count',
+ u'comment_id',
+ u'like_count',
+ u'message',
+ u'parent_id',
+ u'post_id',
+ u'user.id',
+ u'user.name']].to_csv('comments.csv', encoding='utf-8', index = False)
+
+posts.to_csv('posts.csv', encoding='utf-8', index = False)
